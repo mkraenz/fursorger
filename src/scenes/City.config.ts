edf95b1dfcc20110158e0277f0dbf07c60bd193d
Matthias <matthiastutor@outlook.de>
2019-08-28 12:19:58 +0200

    ADD #17 show city info

--- src/scenes/City.config.ts
+++ src/scenes/City.config.ts
@@ -3,11 +3,11 @@ import { CityName } from "./CityName";
 export const cityConfig = {
     [CityName.Athens]: {
         x: 100,
-        y: 50,
+        y: 100,
     },
     [CityName.Bern]: {
         x: 500,
-        y: 50,
+        y: 100,
     },
     [CityName.Cairo]: {
         x: 100,
--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -9,6 +9,10 @@ import { LogicBuilder } from "./logicBuilder";
 
 const PLAYER_INFO_X = 680;
 
+const textStyle = {
+    font: "48px Arial",
+    fill: "#000000",
+};
 export class MainScene extends Scene {
     private player!: IPlayer;
     private graph!: Graph;
@@ -28,6 +32,12 @@ export class MainScene extends Scene {
         this.load.image("Cairo", "./assets/images/cairo3.png");
         this.load.image("background", "./assets/images/background500x300.png");
         this.load.image("backpack", "./assets/images/backpack64x64.png");
+        this.load.image("stock", "./assets/images/storage64x64.png");
+        this.load.image("production", "./assets/images/factory64x64.png");
+        this.load.image(
+            "consumption",
+            "./assets/images/decreasing-bars64x64.png"
+        );
         this.load.audio("background", "./assets/sounds/bgm.mp3");
     }
 
@@ -41,25 +51,42 @@ export class MainScene extends Scene {
         this.drawEdges();
         this.addCities();
         this.add.image(PLAYER_INFO_X, 40, "backpack");
-        this.playerInfo = this.add.text(PLAYER_INFO_X + 40, 15, "", {
-            font: "48px Arial",
-            fill: "#000000",
-        });
+        this.playerInfo = this.add.text(PLAYER_INFO_X + 40, 15, "", textStyle);
 
         this.locationText = this.add.text(
             300,
             300,
             this.player.getLocationName(),
-            {
-                font: "48px Arial",
-                fill: "#000000",
-            }
+            textStyle
         );
     }
 
     public update() {
         this.locationText.setText(this.player.getLocationName());
         this.playerInfo.setText(this.player.stock.toString());
+        this.containerArray.forEach(container => {
+            // getAt(1) returns the stock text
+            (container.getAt(1) as GameObjects.Text).setText(
+                getNode(
+                    this.graph,
+                    container.name as CityName
+                ).economy.stock.toString()
+            );
+            // getAt(2) returns the production text
+            (container.getAt(2) as GameObjects.Text).setText(
+                getNode(
+                    this.graph,
+                    container.name as CityName
+                ).economy.production.toString()
+            );
+            // getAt(3) returns the consupmtion text
+            (container.getAt(3) as GameObjects.Text).setText(
+                getNode(
+                    this.graph,
+                    container.name as CityName
+                ).economy.consumption.toString()
+            );
+        });
     }
 
     private addBackgroundMusic() {
@@ -80,10 +107,22 @@ export class MainScene extends Scene {
         this.containerArray = [];
         Object.values(CityName).forEach(xName => {
             const name = xName as CityName;
-            const cityButton = this.add.image(0, 0, name);
+            const button = this.add.image(0, 0, name);
+            const stock = this.add.image(0, -60, "stock");
+            const stockText = this.add.text(40, -60, "", textStyle);
+            const production = this.add.image(0, 60, "production");
+            const prodText = this.add.text(40, 60, "", textStyle);
+            const consumption = this.add.image(130, 0, "consumption");
+            const consText = this.add.text(170, 0, "", textStyle);
             const config = cityConfig[name];
             const container = this.add.container(config.x, config.y, [
-                cityButton,
+                button,
+                stockText,
+                prodText,
+                consText,
+                stock,
+                production,
+                consumption,
             ]);
             container.setName(name);
             this.containerArray.push(container);
