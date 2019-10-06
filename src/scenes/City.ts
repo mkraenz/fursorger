074932b032ea5728a266664f1383f16d2d1c712f
Matthias <matthiastutor@outlook.de>
2019-10-06 02:15:53 +0200

    REFACTOR #49 not necessary city method

--- src/scenes/City.ts
+++ src/scenes/City.ts
@@ -11,10 +11,6 @@ export class City implements ICity, ILocation {
         }
     ) {}
 
-    public setFactory() {
-        this.economy.production += 1;
-    }
-
     public economize() {
         this.economy.stock += this.economy.production;
     }
