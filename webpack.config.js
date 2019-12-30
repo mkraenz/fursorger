3c623899ae729f0b11a5f5859d5a29da4f9503bb
Mirco Kraenz <kraenzmirco@gmail.com>
2019-12-30 16:51:07 +0100

    CONFIG #96 prefer localhost over 127.0.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,25 @@ module.exports = {
     entry: "./src/index.ts",
     output: {
         path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
+        filename: "bundle.js",
     },
     module: {
         rules: [
             { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" },
-            { test: /phaser\.js$/, loader: "expose-loader?Phaser" }
-        ]
+            { test: /phaser\.js$/, loader: "expose-loader?Phaser" },
+        ],
     },
     devServer: {
         contentBase: path.resolve(__dirname, "./"),
         publicPath: "/build/",
-        host: "127.0.0.1",
+        host: "localhost",
         port: 8080,
-        open: true
+        open: true,
     },
     resolve: {
         extensions: [".ts", ".js"],
         alias: {
-            phaser: phaser
-        }
-    }
+            phaser: phaser,
+        },
+    },
 };
