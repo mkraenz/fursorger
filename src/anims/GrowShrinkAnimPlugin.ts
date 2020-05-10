4b274d34d99d3019a8a1ee7943fcb19187fdf01c
Mirco Kraenz <kraenzmirco@gmail.com>
2020-05-10 15:08:57 +0900

    FIX #184 growshrink anim always works + works for containers

--- src/anims/GrowShrinkAnimPlugin.ts
+++ src/anims/GrowShrinkAnimPlugin.ts
@@ -1,66 +1,75 @@
-import { GameObjects, Scene, Tweens } from "phaser";
-import { ScalableTweenBuilderConfig } from "./ScalableTweenBuilderConfig";
+import { GameObjects, Scene } from "phaser";
 
 enum State {
     pointerover,
     pointerout,
 }
 
-/** Warning: Currently not working together with Containers. */
-export class GrowShrinkAnimPlugin {
+const MsPerSec = 1000;
+
+export class GrowShrinkAnimPlugin extends GameObjects.GameObject {
     public state = State.pointerout;
     private baseScale: number;
-    private growAnim: Tweens.Tween;
-    private shrinkAnim: Tweens.Tween;
+    private maxScale: number;
 
     constructor(
         scene: Scene,
         private target: GameObjects.Image | GameObjects.Text,
-        private maxScale = 1.25,
+        maxRelativeScale = 1.25,
+        private speed = 2.3,
         private debug = false
     ) {
+        super(scene, "GrowShrinkAnimPlugin");
+        scene.add.existing(this);
         this.baseScale = this.target.scale;
-        this.growAnim = scene.add.tween(this.getTweenCfg("grow"));
-        this.shrinkAnim = scene.add.tween(this.getTweenCfg("shrink"));
+        this.maxScale = this.baseScale * maxRelativeScale;
         this.target.on("pointerout", () => this.setPointerOutState());
         this.target.on("pointerover", () => this.setPointerOverState());
     }
 
+    public preUpdate(time: number, delta: number) {
+        if (
+            this.state === State.pointerout &&
+            this.target.scale > this.baseScale
+        ) {
+            this.shrink(delta);
+        }
+        if (
+            this.state === State.pointerover &&
+            this.target.scale < this.maxScale
+        ) {
+            this.grow(delta);
+        }
+    }
+
+    private grow(delta: number) {
+        this.target.setScale(
+            this.target.scaleX + this.speed * (delta / MsPerSec),
+            this.target.scaleY + this.speed * (delta / MsPerSec)
+        );
+    }
+
     private setPointerOutState() {
         this.state = State.pointerout;
-        this.growAnim.pause();
-        this.shrinkAnim.restart();
-
         if (this.debug) {
             // tslint:disable-next-line: no-console
             console.log("pointerout");
         }
     }
 
+    private shrink(delta: number) {
+        this.target.setScale(
+            this.target.scaleX - this.speed * (delta / MsPerSec),
+            this.target.scaleY - this.speed * (delta / MsPerSec)
+        );
+    }
+
     private setPointerOverState() {
         this.state = State.pointerover;
-        this.shrinkAnim.pause();
-        this.growAnim.restart();
 
         if (this.debug) {
             // tslint:disable-next-line: no-console
             console.log("pointerover");
         }
-    }
-
-    private getTweenCfg(
-        direction: "shrink" | "grow"
-    ): ScalableTweenBuilderConfig {
-        const targetScale =
-            direction === "shrink"
-                ? this.baseScale
-                : this.baseScale * this.maxScale;
-        return {
-            targets: this.target,
-            scaleX: targetScale,
-            scaleY: targetScale,
-            ease: "Linear",
-            duration: 100,
-        };
     }
 }
