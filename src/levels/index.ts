dd62de2ff3856877f278dc805f25d5e0363b6f98
Mirco Kraenz (via Travis CI) <kraenzmirco@gmail.com>
2020-05-22 08:33:19 +0000

    Deploy proSingularity/fursorger to github.com/proSingularity/fursorger.git:gh-pages

--- src/levels/index.ts
+++ src/levels/index.ts
@@ -2,5 +2,6 @@ import { level1 } from "./level1";
 import { level2 } from "./level2";
 import { level3 } from "./level3";
 import { level4 } from "./level4";
+import { tutorialLevel1 } from "./tutorialLevel1";
 
-export const levels = [level1, level2, level3, level4];
+export const levels = [tutorialLevel1, level1, level2, level3, level4];
---
+++ src/levels/tutorialLevel1.ts
@@ -0,0 +1,11 @@
+import { ILevel } from "./ILevel";
+
+export const tutorialLevel1: ILevel = {
+    cities: [
+        { name: "Orsus", stock: 1, production: -1, x: 775, y: 430 },
+        { name: "Copia", stock: 2, production: 1, x: 425, y: 333 },
+    ],
+    travelPaths: [{ first: "Orsus", second: "Copia" }],
+    player: { stock: 3, location: "Copia" },
+    background: "map-tutorial",
+};
