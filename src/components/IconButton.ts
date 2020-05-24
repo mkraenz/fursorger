a1ee96a1101f9795a21b2794c19162ab83bbac75
Mirco Kraenz <kraenzmirco@gmail.com>
2020-05-24 20:59:40 +0900

    FIX #234 hardcode svg width and height in the svg

--- src/components/IconButton.ts
+++ src/components/IconButton.ts
@@ -6,20 +6,19 @@ export class IconButton extends GameObjects.Image {
     private icon: GameObjects.Image & { baseScale: number };
     private readonly baseScale = 0.5;
 
+    /** iconCfg.baseScale must be 1 due to rendering of thin border lines when svg widht or height become non-integer  */
     constructor(
         scene: Scene,
         onPointerUp: () => void,
         x: number,
         y: number,
-        iconCfg: { baseScale: number; texture: string }
+        iconCfg: { baseScale: 1; texture: string }
     ) {
         super(scene, x, y, "octagon");
         scene.add.existing(this);
         this.setInteractive();
         this.on("pointerup", onPointerUp);
-        const icon = this.scene.add
-            .image(x - 5, y - 5, iconCfg.texture)
-            .setScale(iconCfg.baseScale);
+        const icon = this.scene.add.image(x, y, iconCfg.texture);
         this.icon = assign(icon, { baseScale: iconCfg.baseScale });
         this.setScale(this.baseScale);
         new GrowShrinkAnimPlugin(scene, this);
