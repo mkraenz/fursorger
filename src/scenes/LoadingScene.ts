e1aa83002469304958873f3c406b0e63e2a01a25
Mirco Kraenz <kraenzmirco@gmail.com>
2022-09-03 18:29:27 +0200

    FIX #354 cannot read property on sys error

--- src/scenes/LoadingScene.ts
+++ src/scenes/LoadingScene.ts
@@ -138,7 +138,6 @@ export class LoadingScene extends Scene {
             } else {
                 this.scene.add('TitleScene', TitleScene, true);
             }
-            this.scene.remove(this);
         });
     }
 
