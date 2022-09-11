e82c0332de2972829c664a16f5178b8d631df74b
knoffi <matthiastutor@outlook.de>
2022-09-11 12:46:12 +0200

    UPDATE #263 tutorial map got secrets

--- src/levels/tutorialLevel1.ts
+++ src/levels/tutorialLevel1.ts
@@ -1,15 +1,18 @@
 import { ILevel } from './ILevel';
 
+const RICH_CITY = "Copia"
+const POOR_CITY = "Orsus"
+
 export const tutorialLevel1: ILevel = {
     name: 'First steps',
     cities: [
-        { name: 'Orsus', stock: 1, production: -1, x: 775, y: 430 },
-        { name: 'Copia', stock: 1, production: 1, x: 425, y: 333 },
+        { name: POOR_CITY, stock: 1, production: -1, x: 775, y: 430 },
+        { name: RICH_CITY, stock: 1, production: 1, x: 425, y: 333 },
     ],
     travelPaths: [
         {
-            first: 'Orsus',
-            second: 'Copia',
+            first: POOR_CITY,
+            second: RICH_CITY,
             points: [
                 { x: 442, y: 346 },
                 { x: 776, y: 432 },
@@ -18,7 +21,11 @@ export const tutorialLevel1: ILevel = {
     ],
     player: {
         stock: 1,
-        location: 'Copia',
+        location: RICH_CITY,
     },
     background: 'map-tutorial',
+    secrets: [
+        { text: `${RICH_CITY}'s fertile lands seem to withstand the Fiery Rain... for now.`, centerX: 336, centerY: 242, height: 50, width: 50 },
+        { text: `${POOR_CITY} suffers from the Crisis\n and its reserves are dwindling.`, centerX: 713, centerY: 467, height: 30, width: 50 },
+    ]
 };
